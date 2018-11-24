4e10b34d23302af7cabe49508727579245d81c54
shayesta <parveenreehan.s@husky.neu.edu>
2018-11-23 21:39:35 -0500

    composition 3

--- untitled/src/exercises/Animal.java
+++
@@ -1,5 +0,0 @@
-public class Animal {
-    public void introduceYourself(){
-        System.out.println("Morr i am an animal");//"Morr. I am an animal."
-    }
-}
--- untitled/src/exercises/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog {
-    Dog(String b) {
-        introduceYourself();
-    }
-
-    public void introduceYourself() {
-        System.out.println("Woof. I am a dog. My name is " );
-    }
-}
